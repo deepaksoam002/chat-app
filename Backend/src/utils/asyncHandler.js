678684957ea588816c33146b5d95c72cd51de518
deepaksoam002 <deepaksoam319@gmail.com>
2026-08-11 18:10:09 +0530

    Add utility classes for API error handling and response formatting

---
+++ Backend/src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+
+
+const asyncHandler = (asyncFunction) => {
+
+    return (req, res, next) => {
+
+        Promise.resolve(asyncFunction(req, res, next)).catch((error) => next(error))
+    }
+}
+
+module.exports = {
+    asyncHandler
+}
